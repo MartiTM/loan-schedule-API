35b3b4916274ac9596b78ae1918ae4385ae3d934
Thomas <thomas.2905@hotmail.fr>
2022-12-26 15:09:14 +0100

    loan calculations interfaces + basic test

---
+++ utils/calc.go
@@ -0,0 +1,7 @@
+package utils
+
+func GetInterestRatePerTerm(annualInterestRate float64) float64
+
+func GetDueDateAmount(totalMoney int, interestRate float64, terms int) int
+
+func GetInterestAmountByTerm(remainingMoney int, interestRate float32) int
---
+++ utils/test_calc.go
@@ -0,0 +1,27 @@
+package utils
+
+import "testing"
+
+func TestGetInterestRatePerTermBasic(t *testing.T) {
+	ans := GetInterestRatePerTerm(0.0425)
+	wanted := 0.0425/12
+	if ans != wanted {
+		t.Errorf("GetInterestRatePerTerm(0.0425) = %v; want %v", ans, wanted)
+	}
+}
+
+func TestGetDueDateAmountBasic(t *testing.T) {
+	ans := GetDueDateAmount(5000000, 0.0425, 4)
+	wanted := 1261087
+	if ans != wanted {
+		t.Errorf("GetDueDateAmount(5000000, 0.0425, 4) = %v; want %v", ans, wanted)
+	}
+}
+
+func TestGetInterestAmountByTermBasic(t *testing.T) {
+	ans := GetInterestAmountByTerm(5000000, 0.0425/12)
+	wanted := 17708
+	if ans != wanted {
+		t.Errorf("GetInterestAmountByTerm(5000000, 0.0425/12) = %v; want %v", ans, wanted)
+	}
+}
