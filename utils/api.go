557a3b283c6170f62c98909b35815eacfdbef350
Thomas <thomas.2905@hotmail.fr>
2023-01-02 16:00:44 +0100

    add CORS

--- utils/api.go
+++ utils/api.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func ReturnJsonResponse(res http.ResponseWriter, httpCode int, resMessage []byte) {
 	res.Header().Set("Content-type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
 	res.WriteHeader(httpCode)
 	res.Write(resMessage)
 }
